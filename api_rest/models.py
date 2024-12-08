df86251d572fe73d844f008beb6b40b23a123fa5
Júlia Diniz Valadares <juliadinizvaladares@hotmail.com>
2024-12-08 14:53:16 -0300

    criate models

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    user_nickname = models.CharField(primary_key=True, max_length=100, default=" ")
+    user_nome = models.CharField(max_length=180, default=" ")
+    user_email = models.EmailField(default=" ")
+    user_idade = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Nickname: {self.user_nickname} | Email: {self.user_email}'
+    
